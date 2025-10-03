eebcda2d287f24f788e2a59063477e5246e71aea
Óskar Jörgen Sandholt <oskarjorgen@gmail.com>
2025-10-03 01:08:02 +0000

    use builtin pwd

--- src/prompt.fish
+++ src/prompt.fish
@@ -1,15 +1,15 @@
-function pretty_path
-  echo (pwd | sed "s|^$HOME|~|g")
-end
-
+# set git prompt flags
 set -g __fish_git_prompt_show_informative_status 1
 set -g __fish_git_prompt_showcolorhints 1
 set -g __fish_git_prompt_showuntrackedfiles 1
 set -g __fish_git_prompt_showdirtystate 1
 
+# set pwd flags
+set -g fish_prompt_pwd_dir_length 0
+
 function fish_prompt
   # whoami pwd git-branch git-status
-  echo (set_color 'blue')(whoami) (set_color 'yellow')(pretty_path) (set_color 'normal')(fish_git_prompt)
+  echo (set_color 'blue')(whoami) (set_color 'yellow')(prompt_pwd) (set_color 'normal')(fish_git_prompt)
   echo (set_color 'red')"\$ "(set_color 'normal')
 end
 
