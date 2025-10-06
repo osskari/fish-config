5e061a383d1c73e3bb46072f6789c154ceba1108
Óskar Jörgen Sandholt <oskar.j.sandholt@origo.is>
2025-10-06 12:50:34 +0000

    add vi keybindings

--- src/prompt.fish
+++ src/prompt.fish
@@ -7,6 +7,9 @@ set -g __fish_git_prompt_showdirtystate 1
 # set pwd flags
 set -g fish_prompt_pwd_dir_length 0
 
+# enable vi mode
+set -g fish_key_bindings fish_vi_key_bindings
+
 function fish_prompt
   # whoami pwd git-branch git-status
   echo (set_color 'blue')(whoami) (set_color 'yellow')(prompt_pwd) (set_color 'normal')(fish_git_prompt)
